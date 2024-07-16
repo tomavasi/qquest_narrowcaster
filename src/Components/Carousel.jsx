3f55594bdd2071a4008edad86f4919137737593d
Mark <markkranenburg@hotmail.nl>
2024-07-16 09:59:08 +0200

    add

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,8 +1,28 @@
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+
 export const Carousel = () => {
-    
+    const numbers = [1,2,3];
+
+    const settings = {
+        dots: false,
+        Infinite: true,
+        arrows: false,
+        speed: 2000,
+        autoplay: true,
+        autoplaySpeed: 10000
+    }
+
     return (
         <div className="widget carousel">
             <h1>Carousel</h1>
+            <Slider {...settings}>
+                {numbers.map((item) => (
+                    <p>{item}</p>
+                ))}
+            </Slider>
         </div>
     )
 }
