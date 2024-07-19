1aca6a3ef621735fd9ff42a58cd00a4d8e949c54
Vasilis <tomaras.vasi@outlook.com>
2024-07-19 15:03:13 +0200

    proxy testen

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const NS_KEY = 'c9af8962e10e46bc92f8e98b501a3894';
 
 export default function handler(req, res) {
   const proxy = createProxyMiddleware({
-    target: 'https://gateway.apiportal.ns.nl/',
+    target: 'https://gateway.apiportal.ns.nl/nsapp-stations/v2/nearest?lat=52.08253517526941&lng=5.117591126554095&limit=2&includeNonPlannableStations=false',
     changeOrigin: true,
     pathRewrite: {
       '^/api': '',
--- src/API-requests/NS-API.jsx
+++ src/API-requests/NS-API.jsx
@@ -1,15 +1,16 @@
 const NS_KEY = 'c9af8962e10e46bc92f8e98b501a3894';
-const stationsUrl =  '/api/nsapp-stations/v2/nearest?lat=52.08253517526941&lng=5.117591126554095&limit=2&includeNonPlannableStations=false';
+const stationsUrl =  '/api';
 
 export async function getNearByStations () {
     const response = await fetch(`${stationsUrl}`, {
-        method: 'GET',
-        // Request headers
-        headers: {
-            'Cache-Control': 'no-cache',
-            'Ocp-Apim-Subscription-Key': `${NS_KEY}`,}
+        // method: 'GET',
+        // // Request headers
+        // headers: {
+        //     'Cache-Control': 'no-cache',
+        //     'Ocp-Apim-Subscription-Key': `${NS_KEY}`,}
     })
     if(response.ok){
+        console.log(response.ok)
         return response.json();
     }
     throw new Error('Failed to fetch NearBy Stations API');
