50215c27ca39bd8d21758e795d208764e60ab0f0
Vasilis <tomaras.vasi@outlook.com>
2024-07-19 14:53:51 +0200

    headers toegevoegd bij proxy

--- api/index.js
+++ api/index.js
@@ -1,11 +1,19 @@
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
+const NS_KEY = 'c9af8962e10e46bc92f8e98b501a3894';
+
 export default function handler(req, res) {
   const proxy = createProxyMiddleware({
-    target: 'https://gateway.apiportal.ns.nl',
+    target: 'https://gateway.apiportal.ns.nl/',
     changeOrigin: true,
     pathRewrite: {
       '^/api': '',
+    },
+    onProxyReq: (proxyReq, req, res) => {
+      // Add your custom headers here
+      proxyReq.setHeader('Ocp-Apim-Subscription-Key', `${NS_KEY}`);
+      proxyReq.setHeader( 'Cache-Control', 'no-cache');
+      // Add any other headers you need
     },
   });
 
